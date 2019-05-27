5e2146493610bc32493464a4df3289ee43a92ed7
AdrienCHABERT <adrien.chabert@etu.unige.ch>
2019-05-27 10:49:27 +0200

    petite mise a jour

--- traitement.py
+++ traitement.py
@@ -53,6 +53,38 @@ class Traitement(object):
             df.iloc[int(i),6] = j*30
             j = j + 1
         return df
+    
+    def arrosageHist(self,df,pot):
+        df['Arrosage'] = df.index
+        df['TAfterArrosage'] = df.index
+        df['ArrosageHist'] = df.index
+        aro = [0,0,0,0]
+        if pot == 1:
+            aro = [10,20,40,35]
+        elif pot == 2:
+            aro = [10,15,30,20]
+        j = 0
+        nligne = df.shape[0]
+        df.iloc[:,5] = 0
+        tmp = 0
+        for i in range(nligne):
+            if i in df.loc[df['moistureAdd'] >= 3].index:
+                if pd.to_datetime(df.iloc[i,0]) < datetime.datetime(2019, 3, 28,0,0,0):
+                    df.iloc[int(i),5] = aro[0]
+                elif pd.to_datetime(df.iloc[i,0]) < datetime.datetime(2019, 4, 18,0,0,0):
+                    df.iloc[int(i),5] = aro[1]
+                elif pd.to_datetime(df.iloc[i,0]) < datetime.datetime(2019, 5, 3,0,0,0):
+                    df.iloc[int(i),5] = aro[2]
+                else:
+                    df.iloc[int(i),5] = aro[3]
+                j = 0
+                tmp = df.iloc[int(i),5]
+
+            df.iloc[int(i),6] = j*30
+            df.iloc[i,7] = tmp
+            j = j + 1
+        return df
+
 
     def eliminateNaNValue(self,df,i):
         nligne = df.shape[0]
