d4590cb9a6aedc5ecabe6bae6822e237cbc18990
AdrienCHABERT <adrien.chabert@etu.unige.ch>
2019-05-28 11:54:15 +0200

    Randomization des tests

--- Prediction.py
+++ Prediction.py
@@ -6,90 +6,59 @@ import numpy as np
 import pandas as pd
 import datetime
 from traitement import Traitement
+import random
+
 
 t= Traitement()
 
 
-df = t.preparation('DataAll.csv', 37)
+df = t.preparation('DataDemeterAll.csv', 6)
 nligne = df.shape[0]
 nColumn = df.shape[1]
 
-
+#[df, dataTest] = t.partitionTest(df,7)
+#print(df.shape[0])
 #data test
 
-dfTest = t.preparation('Data30AprilTo8Mai.csv', 17)
-nligneTest = dfTest.shape[0]
 
 #Separation of the data in 3 parts
+limite = 450
 dfArrosage = df.loc[df['TAfterArrosage'] == 0]
 tmp = df.loc[df['TAfterArrosage'] > 0]
-dfStabilisation = df.loc[df['TAfterArrosage'] > 450]
-dfEvaporation = tmp.loc[df['TAfterArrosage'] <= 450]
+dfStabilisation = df.loc[df['TAfterArrosage'] > limite]
+dfEvaporation = tmp.loc[df['TAfterArrosage'] <= limite]
 
 #Linear Regression
 reg = linear_model.LinearRegression()
 reg.fit(df[['mean_moisture-percent','mean_temperature','Arrosage']],df.moistureAdd)
-# regLinearAro = linear_model.LinearRegression()
-# regLinearAro.fit(dfArrosage[['mean_moisture-percent','Arrosage']],dfArrosage.moistureAdd)
-# regLinearAro.intercept_
-# regLinearAro.coef_
-#
-# regLinearEva = linear_model.LinearRegression()
-# regLinearEva.fit(dfEvaporation[['mean_moisture-percent','mean_temperature','TAfterArrosage','ArrosageHist']],dfEvaporation.moistureAdd)
-# regLinearEva.intercept_
-# regLinearEva.coef_
-#
-# regLinearSta = linear_model.LinearRegression()
-# regLinearSta.fit(dfStabilisation[['mean_moisture-percent','mean_temperature','TAfterArrosage']],dfStabilisation.moistureAdd)
-# regLinearSta.intercept_
-# regLinearSta.coef_
 
+# Regression on the 3 parts
+regLinearAro = linear_model.LinearRegression()
+regLinearAro.fit(dfArrosage[['mean_moisture-percent','Arrosage']],dfArrosage.moistureAdd)
+regLinearAro.intercept_
+regLinearAro.coef_
 
-#Test de nos données
-# init = 0
-# base = dfTest
-# dfTest['resultSepare'] = dfTest.index
-# StartHumidity = base.iloc[init,1]
-# position = dfTest.shape[1]-1
-# TestSize = nligneTest
-# ErrAro = 0
-# result = np.zeros(TestSize)
-# ErrEva = 0
-# nAro = 0
-# for i in range(0,TestSize):
-#     dfTest.iloc[int(i+init),position] = StartHumidity
-#     if i%48 == 0:
-#         nAro =+ 1
-#         StartHumidity = dfTest.iloc[init+i,1]
-#         result[i] = StartHumidity + regLinearAro.predict([[StartHumidity,base.iloc[init+i,5]]])[0]
-#         ErrAro =+ dfTest.iloc[int(i),position] - base.iloc[i,1]
-#     elif i%48 <= 15:
-#         result[i] = StartHumidity + regLinearEva.predict([[StartHumidity,base.iloc[init+i,2],base.iloc[init+i,6],base.iloc[init+i,7]]])[0]
-#         ErrEva =+ dfTest.iloc[int(i),position] - base.iloc[i,1]
-#     else:
-#         result[i] = StartHumidity + regLinearSta.predict([[StartHumidity,base.iloc[init+i,2],base.iloc[init+i,6]]])[0]
-#         ErrEva =+ dfTest.iloc[int(i),position] - base.iloc[i,1]
-#     StartHumidity = result[i]
-#
-# #Our erreur. 1 is for the watering and the other is for the period of evaporation
-# ErrAro = ErrAro/nAro
-# ErrEva = ErrEva/(TestSize-nAro)
-#
-# print("Moyenne Erreur d'Arrosage : ", ErrAro)
-# print("Moyenne d'erreur evaporation : ", ErrEva)
-# ErrSepare = np.mean(dfTest.iloc[:,1].values-dfTest.iloc[:,position].values)
-# print("Moyenne d'erreur : ",ErrSepare)
+regLinearEva = linear_model.LinearRegression()
+regLinearEva.fit(dfEvaporation[['mean_moisture-percent','mean_temperature','TAfterArrosage']],dfEvaporation.moistureAdd)
+regLinearEva.intercept_
+regLinearEva.coef_
 
+regLinearSta = linear_model.LinearRegression()
+regLinearSta.fit(dfStabilisation[['mean_moisture-percent','mean_temperature','TAfterArrosage']],dfStabilisation.moistureAdd)
+regLinearSta.intercept_
+regLinearSta.coef_
 
 
 #Creation de l'algorithme pour predire l'arrosage
-[res, bestaro] = t.prediction(reg, 22,26,23,22)
-print(bestaro)
-print(res)
+# Avec une simple regression :
+#[res, bestaro] = t.prediction(reg, 22,26,23,22)
+
+# Avec la separation en 3 de la journee:
+[res, bestaro] = t.prediction3(regLinearAro, regLinearEva, regLinearSta, limite, 21,26 ,23,23)
+
+print("Meilleure arrosage : ", bestaro)
+print("Evolution de l'humidite : ", res)
 plt.plot(res)
 plt.ylabel('moisture percent')
 plt.xlabel('iteration')
 plt.show()
-
-
-print(t.pure(df))
--- traitement.py
+++ traitement.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import datetime
+import random
+
 
 
 
@@ -58,11 +60,11 @@ class Traitement(object):
         df['Arrosage'] = df.index
         df['TAfterArrosage'] = df.index
         df['ArrosageHist'] = df.index
-        aro = [0,0,0,0]
+        aro = [0,0,0,0,0]
         if pot == 1:
-            aro = [10,20,40,35]
+            aro = [10,20,40,35,45]
         elif pot == 2:
-            aro = [10,15,30,20]
+            aro = [10,15,30,20,10]
         j = 0
         nligne = df.shape[0]
         df.iloc[:,5] = 0
@@ -75,8 +77,11 @@ class Traitement(object):
                     df.iloc[int(i),5] = aro[1]
                 elif pd.to_datetime(df.iloc[i,0]) < datetime.datetime(2019, 5, 3,0,0,0):
                     df.iloc[int(i),5] = aro[2]
-                else:
+                elif pd.to_datetime(df.iloc[i,0]) < datetime.datetime(2019, 5, 23,0,0,0):
                     df.iloc[int(i),5] = aro[3]
+                else:
+                    df.iloc[int(i),5] = aro[4]
+
                 j = 0
                 tmp = df.iloc[int(i),5]
 
@@ -132,6 +137,30 @@ class Traitement(object):
             print(el, res[48])
         return evalBest, aroBest
 
+    def prediction3(self,regAro, regEva, regSta,limite,start,highTemp,lowTemp,purpose):
+        val = [5,10,15,20,25,30,35,40]
+        resBest = 1000
+        evalBest = []
+        aroBest = 0
+        for el in val:
+            res = np.zeros(49)
+            res[0] = start
+            res[1] = res[0] + regAro.predict([[res[0],el]])[0]
+            reg = regEva
+            for i in range(2,49):
+                if i == limite//30:
+                    reg = regSta
+                if i <= 24:
+                    res[i] = res[i-1] + reg.predict([[res[i-1],highTemp,i*30]])[0]
+                else:
+                    res[i] = res[i-1] + reg.predict([[res[i-1],lowTemp,i*30]])[0]
+            if abs(resBest-purpose) > abs(res[48]-purpose):
+                resBest = res[48]
+                evalBest = res
+                aroBest = el
+            print(el, res[48])
+        return evalBest, aroBest
+
     def preparation(self, name, init):
         df = pd.read_csv(name)
         #Création des deux autres colonnes
@@ -141,3 +170,17 @@ class Traitement(object):
         #Delete the data that are not inside a loop of one day
         df.drop(df.index[range(0,init)],axis = 0,inplace = True)
         return df
+
+    def partitionTest(self, df, njour):
+        nligne = df.shape[0]
+        jour = nligne //48
+        incr = 0
+        jourTest = random.sample(range(1,jour),k=njour)
+        jourTest.sort()
+        indTest = []
+        for el in jourTest:
+            indTest = np.append(indTest,range(int(48*(el-1)),int(48*el)))
+            indTest = indTest.astype(int)
+        dataTest = df.iloc[indTest,:].copy()
+        df.drop(df.index[indTest],axis = 0,inplace = True)
+        return df, dataTest
